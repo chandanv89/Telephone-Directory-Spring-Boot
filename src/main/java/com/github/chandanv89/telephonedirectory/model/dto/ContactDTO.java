e6295506d8e24e02e0c00f842df37257cd417f74
Chandan Veerabhadrappa <chandanv89@gmail.com>
2019-01-05 22:07:17 +0000

    Updates a lot of things! ;) 	new file:   src/main/java/com/github/chandanv89/telephonedirectory/model/dto/ContactDTO.java 	new file:   src/main/resources/smple.json 	new file:   src/test/java/com/github/chandanv89/telephonedirectory/controller/TestContactsController.java 	modified:   pom.xml 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/controller/ContactsController.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/controller/helper/ContactsControllerHelper.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/model/ApiResponse.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/model/ApplicationException.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/model/Contact.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/model/dto/ContactDTO.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/repository/InitialSeeder.java 	modified:   src/main/java/com/github/chandanv89/telephonedirectory/utility/Utilities.java 	modified:   src/main/resources/smple.json 	modified:   src/test/java/com/github/chandanv89/telephonedirectory/controller/TestContactsController.java 	src/main/resources/data.json

---
+++ src/main/java/com/github/chandanv89/telephonedirectory/model/dto/ContactDTO.java
@@ -0,0 +1,4 @@
+package com.github.chandanv89.telephonedirectory.model.dto;
+
+public class ContactDTO {
+}
---
+++ src/test/java/com/github/chandanv89/telephonedirectory/controller/TestContactsController.java
@@ -0,0 +1,33 @@
+package com.github.chandanv89.telephonedirectory.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestContactsController {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
